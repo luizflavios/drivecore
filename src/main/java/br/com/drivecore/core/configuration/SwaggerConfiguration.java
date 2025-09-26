a63bcf648df34f178be7e0dbd995138a549a6988
luizflavios <ngpbrasil@gmail.com>
2025-09-26 10:28:42 -0300

    wip

--- src/main/java/br/com/drivecore/core/configuration/SwaggerConfiguration.java
+++ src/main/java/br/com/drivecore/core/configuration/SwaggerConfiguration.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -16,6 +19,7 @@ public class SwaggerConfiguration {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(Collections.singletonList(new Server().url("/")))
                 .info(new Info().title("Tl Transportadora Api").description("Documentation of api").version("1.0"))
                 .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTHENTICATION))
                 .components(new Components().addSecuritySchemes(BEARER_AUTHENTICATION, createAPIKeyScheme()));
